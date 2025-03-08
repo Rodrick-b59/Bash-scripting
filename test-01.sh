636ecc0a207ec9649d154b920817751dcfa666e1
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-08 04:54:30 -0600

    msh

--- test-01.sh
+++ test-01.sh
@@ -2,3 +2,26 @@
 
 #this is to print something
 echo "devops system"
+
+/print the name of script am executing
+
+echo "05-special variables.sh"
+
+
+# special variables
+# $0 prints the name of the script youre running
+# $1 is the first argument you supply to the script
+# $2 is the second argument you supply to the script
+
+# $0  : is the name of the script you are running
+
+TEAM=$1
+PROJECT=$2
+echo "value of a is $a"
+echo "scriptName is $)"
+
+echo $0    #prints scriptname
+echo "Number Of Arguments In Script are $#"
+echo "Exit Status Of Previous Command $?"
+echo "Arguments In The Script Are $*"
+echo "Arguments In The Script Are $@"

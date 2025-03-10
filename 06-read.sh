acaadc10c6884eff297905403898edf958210b8b
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-10 11:28:05 -0500

    msh

---
+++ 06-read.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo  " this script will proceed only by taking the input from the user interactively on the terminal while executing the script"
+
+read -p "Enter the name of the person running the script: " NAME
+echo "name of the user who executed the script is $NAME"

3ef4e5175426199b7b1264dfaeaf11ef65d3cfda
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-10 11:16:20 -0500

    msh

---
+++ demooncodes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# " ""  : Double quotes are used to suppress the special meaning of characters or to prevent the interpretation of special characters.
+# ' '   : Single quotes are used to suppress the special meaning of characters or to prevent the interpretation of special characters.
+
+a=1000
+echo "Demo on quotes:"
+echo $?
+echo '$?'
+echo "value of is $a"
+echo 'value of a is $a'

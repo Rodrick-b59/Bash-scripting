23165a4d3621ee3d2cfd952ec6cccb7c0b1fe1ed
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-10 18:45:44 -0500

    “Bash-scripting”

---
+++ 08-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#syntax for if condition
+
+
+# if [ condition ]; then
+#     commands
+# fi
+
+#     fi is used when you want to close (end of syntax)
+
+<<COMMENT 
+
+if 
---
+++ 09-if.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "demo on if conditions"
+
+ACTION=$1
+
+if [$ACTION == "start"]; then
+    echo "start the service
+fi
+
+echo -e "Script Completed"
+

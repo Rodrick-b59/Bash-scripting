c23c4ba6046cb4e43fd50202c9d97b379e4d2e4c
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-10 19:10:59 -0500

    Bash scripting ~ Autopush

--- 09-if.sh
+++ 09-if.sh
@@ -4,8 +4,8 @@ echo -e "demo on if conditions"
 
 ACTION=$1
 
-if ["$ACTION" == "start"]; then
-    echo -e "start the service"
+if [ "$ACTION" == "start" ]; then  # the below command will only work if the condition is true (example in this case --if you write start after the filename)
+    echo "start the service"
 fi
 
 echo -e "Script Completed"

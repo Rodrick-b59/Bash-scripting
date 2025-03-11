282f06f19cb8d20b00f5c34d4c861ae849461850
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-10 19:27:34 -0500

    Bash scripting ~ Autopush

---
+++ 10-ifelse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "demo on if conditions"
+
+ACTION=$1
+
+if [ "$ACTION" == "start" ]; then  # the below command will only work if the condition is true (example in this case --if you write start after the filename)
+    echo "start the service"
+else
+    echo "valid arguments are start"
+    echo "EX: bash $0 start"

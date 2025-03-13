4d28831321bd214030bfe22b369cd9833df04651
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-13 18:19:57 -0500

    Bash scripting ~ Autopush

--- 05-redirectors.sh
+++ 05-redirectors.sh
@@ -4,8 +4,15 @@
 
 # example ls -ltr > filename - redietcs the content oe output to the new file
 # example ls -ltr >> filename - appends the content to the file
+# OUTPUTS
+# 1. standard output
 echo "this is a test file" > testfile.txt  # this deletes the content of the file and adds the content
+
+# 2. standard error
  echo "this is a test file" >> testfile.txt    # it add content to the file
+ 
+ Standard output and standard error &> filename or &>> filename
+# example ls -ltr &> filename - redietcs the content of output to the new file
 
 
  #

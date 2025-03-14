456c4cbdd641e421d10158992d5d8e789a658d4d
Rodrick-code-devops <abecherodrick@gmail.com>
2025-03-14 07:57:29 -0500

    “operators”

---
+++ 12-operators.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# expressions are categorized into 3 types and based on the expression, we need to use  operators   
+
+# 1. numbers
+# 2. strings
+# 3. files
+
+# operators on numbers
+# 1. -eq  (equal)
+# 2. -ne  (not equal)
+# 3. -gt  (greater than)
+# 4. -lt  (less than)
+# 5. -ge  (greater than or equal to)
+# 6. -le  (less than or equal to)
+
+# operators on strings
+# 1. =  (equal) eg. a=10  (meaning a is equal to 10)
+# 2. !=  (not equal)  eg a!=10  (meanins a not equal to 10)
+# 
+# 3. -z  (zero length)
+# 4. -n  (non zero length)  # -n is used to check the length of the string
+
+# operators on files
+# 1. -f  (file exists)
+# 2. -d  (directory exists)
+# 3. -s  (file is not empty)
+# 4. -r  (file has read permission)
+# 5. -w  (file has write permission)
+# 6. -x  (file has execute permission)
+
---
+++ 13-loops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Loops are used to execute a set of commands repeatedly until a particular condition is satisfied.
+
+# 1. there are 2 variations of loops in bash
+# 2. for loop (used when you know the number of times you want to execute the commands)
+# 3. while loop (used when you do not know the number of times you want to execute the commands and condition is true)
+
+for i in 1 2 3 4 5 ; do
+    echo "value of i is $i"
+done
+
+
